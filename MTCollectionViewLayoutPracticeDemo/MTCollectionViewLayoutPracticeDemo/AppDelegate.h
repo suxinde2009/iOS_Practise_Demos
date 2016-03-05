798360f81fb7a54b1c5b89030af6a5ed67cb7fa3
suxinde2009 <suxinde2009@126.com>
2016-03-05 14:06:58 +0800

    test

---
+++ MTCollectionViewLayoutPracticeDemo/MTCollectionViewLayoutPracticeDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MTCollectionViewLayoutPracticeDemo
+//
+//  Created by su xinde on 16/3/3.
+//  Copyright © 2016年 com.su. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
